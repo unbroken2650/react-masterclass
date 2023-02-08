be707875e1316a42500067c97ac442017cbe1c5c
unbroken2650 <unbroken2650@gmail.com>
2023-02-08 23:26:36 +0900

    :sparkles: typescript started
    ~3.3

---
+++ ts/src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle borderColor="yellow" bgColor="teal" />
+      <Circle text="I'm here" bgColor="tomato" />
+    </div>
+  );
+}
+
+export default App;
