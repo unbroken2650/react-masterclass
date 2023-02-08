eaafea6db202eafd599c8007b4d87d7f0476e55f
unbroken2650 <unbroken2650@gmail.com>
2023-02-09 01:50:05 +0900

    :sparkles: Starting Coin WebPage
    ~5.0

---
+++ ts/src/routes/Coin.tsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+
+interface Params {
+  coinId: string;
+}
+
+function Coin() {
+  const { coinId } = useParams<Params>();
+  console.log(coinId);
+  return <h1>Coin: {coinId}</h1>;
+}
+
+export default Coin;
