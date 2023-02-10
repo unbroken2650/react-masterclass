eedbfcbbe155b82b2fe7512aad7fafd0bf2e08bb
unbroken2650 <unbroken2650@gmail.com>
2023-02-11 02:44:16 +0900

    :card_file_box: Applied React Query
    ~5.10

--- ts/src/index.tsx
+++ ts/src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { theme } from "./theme";
 const container = document.getElementById("root");
 const root = createRoot(container!);
 
+const queryClient = new QueryClient();
+
 root.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </QueryClientProvider>
 );
