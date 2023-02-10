eed253b0c0fdd75209b388b20d0896a0d2fb51c0
unbroken2650 <unbroken2650@gmail.com>
2023-02-11 02:03:45 +0900

    :card_file_box: Fetched coin data
    ~5.5

--- ts/src/index.tsx
+++ ts/src/index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { theme } from "./theme";
+const container = document.getElementById("root");
+const root = createRoot(container!);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+root.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
 );
