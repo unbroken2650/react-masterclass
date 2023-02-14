ee0c225567cd3f1f9560b71aef8387c29e8b4421
unbroken2650 <unbroken2650@gmail.com>
2023-02-14 23:10:29 +0900

    :bricks: Challenge Completed
    basename of the BrowserRouter

--- ts/src/Router.tsx
+++ ts/src/Router.tsx
@@ -9,7 +9,7 @@ interface IRouterProps {
 
 function Router({ toggleDark, isDark }: IRouterProps) {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/:coinId">
           <Coin isDark={isDark} />
